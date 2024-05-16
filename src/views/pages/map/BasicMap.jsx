01a3e7839494720f3b2bba06ea0af1f065d6a8fa
Nguyen-Huy-Phuc <nguyenhuyphc@gmail.com>
2024-05-16 17:13:26 +0700

    update order for map

--- src/views/pages/map/BasicMap.jsx
+++ src/views/pages/map/BasicMap.jsx
@@ -31,13 +31,52 @@ import AlarmOutlinedIcon from "@mui/icons-material/AlarmOutlined";
 
 export default function BasicMap() {
   const [shippers, setShippers] = useState([]);
+  const [orders, setOrders] = useState([
+    {
+      id: "Order 1",
+      img: "https://images.fpt.shop/unsafe/filters:quality(5)/fptshop.com.vn/uploads/images/tin-tuc/174965/Originals/meme-la-gi-5.jpg",
+      latitude: "10.8750883",
+      longitude: "106.7992",
+      status: "Đang chờ",
+      isActive: true,
+    },
+    {
+      id: "Order 2",
+      img: "https://images.fpt.shop/unsafe/filters:quality(90)/fptshop.com.vn/uploads/images/tin-tuc/174965/Originals/meme-la-gi-3.jpg",
+      latitude: "10.87796",
+      longitude: "106.80108",
+      status: "Đang giao hàng",
+      isActive: false,
+    },
+    {
+      id: "Order 3",
+      img: "https://hoanghamobile.com/tin-tuc/wp-content/uploads/2023/09/meme-che-15.jpg",
+      latitude: "10.87821",
+      longitude: "106.79594",
+      status: "Đã hoàn thành",
+      isActive: false,
+    },
+    {
+      id: "Order 4",
+      img: "https://bizweb.dktcdn.net/100/438/408/files/meme-het-cuu-yody-vn-11.jpg?v=1695455529047",
+      latitude: "10.88032",
+      longitude: "106.79516",
+      status: "Đã hủy",
+      isActive: false,
+    },
+  ]);
   const [showDeliveringShippers, setShowDeliveringShippers] = useState(false);
   const [showOfflineShippers, setShowOfflineShippers] = useState(false);
   const [showAvailableShippers, setShowAvailableShippers] = useState(false);
+  const [showDeliveringOrder, setShowDeliveringOrder] = useState(false);
+  const [showAvailableOrder, setShowAvailableOrder] = useState(false);
+  const [showCancelOrder, setShowCancelOrder] = useState(false);
+  const [showDoneOrder, setShowDoneOrder] = useState(false);
   const [shipperPaths, setShipperPaths] = useState(() => {
     const savedPaths = localStorage.getItem("shipperPaths");
     return savedPaths ? JSON.parse(savedPaths) : {};
   });
+  const [showShipperOrOrder, setShowShipperOrOrder] = useState(true);
   const [selectedShipperId, setSelectedShipperId] = useState(null);
 
   const mapRef = useRef(null);
@@ -48,10 +87,38 @@ export default function BasicMap() {
     setShowOfflineShippers(false);
   };
 
+  const handleShowDeliveringOrders = () => {
+    setShowDeliveringOrder(!showDeliveringOrder);
+    setShowAvailableOrder(false);
+    setShowDoneOrder(false);
+    setShowCancelOrder(false);
+  };
+
   const handleShowAvailableShippers = () => {
     setShowAvailableShippers(!showAvailableShippers);
     setShowDeliveringShippers(false);
     setShowOfflineShippers(false);
+  };
+
+  const handleShowAvailableOrders = () => {
+    setShowAvailableOrder(!showAvailableOrder);
+    setShowDeliveringOrder(false);
+    setShowDoneOrder(false);
+    setShowCancelOrder(false);
+  };
+
+  const handleShowDoneOrders = () => {
+    setShowDoneOrder(!showDoneOrder);
+    setShowDeliveringOrder(false);
+    setShowAvailableOrder(false);
+    setShowCancelOrder(false);
+  };
+
+  const handleShowCancelOrder = () => {
+    setShowCancelOrder(!showCancelOrder);
+    setShowDeliveringOrder(false);
+    setShowAvailableOrder(false);
+    setShowDoneOrder(false);
   };
 
   const handleShowOfflineShippers = () => {
@@ -62,20 +129,33 @@ export default function BasicMap() {
 
   const handleShipperClick = (shipper) => {
     if (selectedShipperId === shipper.id) {
-      // Nếu shipper hiện tại đã được chọn, bỏ chọn và ẩn đường đi
       setSelectedShipperId(null);
     } else {
-      // Nếu chọn shipper mới, cập nhật selectedShipperId và hiển thị đường đi
       setSelectedShipperId(shipper.id);
       const shipperlocation = [shipper.latitude, shipper.longitude];
       mapRef.current.flyTo(shipperlocation, 18);
     }
   };
 
+  const handleOrderClick = (order) => {
+    const orderLocation = [order.latitude, order.longitude];
+    mapRef.current.flyTo(orderLocation, 18);
+  };
+
   const customIcon = new Icon({
-    iconUrl: "https://cdn-icons-png.flaticon.com/128/7541/7541900.png",
+    iconUrl: showShipperOrOrder
+      ? "https://cdn-icons-png.flaticon.com/128/7541/7541900.png"
+      : "https://www.vhv.rs/dpng/d/71-712666_pre-order-icon-png-transparent-png.png",
     iconSize: [38, 38],
   });
+
+  const handleChange = () => {
+    if (showShipperOrOrder) {
+      setShowShipperOrOrder(false);
+    } else {
+      setShowShipperOrOrder(true);
+    }
+  };
 
   const createClusterCustomIcon = function (cluster) {
     return new divIcon({
@@ -124,8 +204,8 @@ export default function BasicMap() {
       }
     };
     fetchData();
-    const intervalId = setInterval(fetchData, 3000);
-    return () => clearInterval(intervalId);
+    // const intervalId = setInterval(fetchData, 3000);
+    // return () => clearInterval(intervalId);
   }, []);
 
   const countShippersByStatus = (status) => {
@@ -134,7 +214,13 @@ export default function BasicMap() {
     ).length;
   };
 
-  const actions = [
+  const countOrderByStatus = (status) => {
+    return orders.filter(
+      (order) => order.status.toLowerCase() === status.toLowerCase()
+    ).length;
+  };
+
+  const actions_1 = [
     {
       icon: <OnlinePrediction />,
       name: `Shipper đang giao hàng (${countShippersByStatus(
@@ -153,6 +239,47 @@ export default function BasicMap() {
       onClick: handleShowOfflineShippers,
     },
   ];
+
+  const actions_2 = [
+    {
+      icon: <OnlinePrediction />,
+      name: `Order đang chờ(${countOrderByStatus("Đang chờ")})`,
+      onClick: handleShowAvailableOrders,
+    },
+    {
+      icon: <AlarmOutlinedIcon />,
+      name: `Order đang giao(${countOrderByStatus("Đang giao hàng")})`,
+      onClick: handleShowDeliveringOrders,
+    },
+    {
+      icon: <WifiTetheringOffIcon />,
+      name: `Order đã giao hàng thành công (${countOrderByStatus(
+        "Đã hoàn thành"
+      )})`,
+      onClick: handleShowDoneOrders,
+    },
+    {
+      icon: <WifiTetheringOffIcon />,
+      name: `Order đã hủy (${countOrderByStatus("Đã hủy")})`,
+      onClick: handleShowCancelOrder,
+    },
+  ];
+
+  const actions = showShipperOrOrder ? actions_1 : actions_2;
+
+  const filterOrders = orders.filter((order) => {
+    if (showAvailableOrder) {
+      return order.status.toLowerCase() === "đang chờ";
+    } else if (showDeliveringOrder) {
+      return order.status.toLowerCase() === "đang giao hàng";
+    } else if (showDoneOrder) {
+      return order.status.toLowerCase() === "đã hoàn thành";
+    } else if (showCancelOrder) {
+      return order.status.toLowerCase() === "đã hủy";
+    } else {
+      return order.isActive;
+    }
+  });
 
   const filteredShippers = shippers.filter((shipper) => {
     if (showDeliveringShippers) {
@@ -178,7 +305,14 @@ export default function BasicMap() {
     <>
       <MapContainer ref={mapRef} center={[10.8387503, 106.8347127]} zoom={13}>
         <button className="toggle-btn" onClick={toggleMapType}>
-          {showSatellite ? "Switch to Default Map" : "Switch to Satellite Map"}
+          {showSatellite ? "Default Map" : "Satellite Map"}
+        </button>
+        <button
+          className="toggle-btn"
+          style={{ left: "13em" }}
+          onClick={handleChange}
+        >
+          {!showShipperOrOrder ? "Shipper" : "Order"}
         </button>
         <TileLayer
           attribution='© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
@@ -213,6 +347,59 @@ export default function BasicMap() {
             />
           ))}
         </SpeedDial>
+
+        {(showAvailableOrder ||
+          showCancelOrder ||
+          showDeliveringOrder ||
+          showDoneOrder) && (
+          <List
+            sx={{
+              width: "100%",
+              maxWidth: 315,
+              bgcolor: "background.paper",
+              borderRadius: "10px",
+              overflowY: "auto",
+              maxHeight: "505px",
+            }}
+            style={{
+              position: "absolute",
+              top: "1%",
+              right: "6.5%",
+              zIndex: 1000,
+            }}
+          >
+            {filterOrders.map((order) => (
+              <React.Fragment key={order.id}>
+                <ListItem
+                  alignItems="flex-start"
+                  button
+                  onClick={() => handleOrderClick(order)}
+                >
+                  <ListItemAvatar>
+                    <Avatar alt={order.id} src={order.img} />
+                  </ListItemAvatar>
+                  <ListItemText
+                    primary={order.id}
+                    secondary={
+                      <>
+                        <Typography
+                          component="span"
+                          variant="body2"
+                          sx={{ display: "inline" }}
+                          color="text.primary"
+                        >
+                          Trạng thái: <StatusBadge status={order.status} />
+                        </Typography>
+                      </>
+                    }
+                  />
+                </ListItem>
+
+                <Divider variant="inset" component="li" />
+              </React.Fragment>
+            ))}
+          </List>
+        )}
 
         {(showDeliveringShippers ||
           showOfflineShippers ||
@@ -272,27 +459,45 @@ export default function BasicMap() {
           chunkedLoading
           iconCreateFunction={createClusterCustomIcon}
         >
-          {filteredShippers.map((shipper) => (
-            <Marker
-              key={shipper.id}
-              position={[shipper.latitude, shipper.longitude]}
-              icon={customIcon}
-              eventHandlers={{
-                click: () => handleShipperClick(shipper),
-              }}
-            >
-              <Popup>
-                <img src={shipper.img} alt={shipper.id} />
-                <h2>{shipper.id}</h2>
-                <p>Kinh độ: {shipper.latitude}</p>
-                <p>Vĩ độ: {shipper.longitude}</p>
-                <p>Biển số xe: {shipper.carindentify}</p>
-                <p>
-                  Trạng thái: <StatusBadge status={shipper.status} />
-                </p>
-              </Popup>
-            </Marker>
-          ))}
+          {showShipperOrOrder
+            ? filteredShippers.map((shipper) => (
+                <Marker
+                  key={shipper.id}
+                  position={[shipper.latitude, shipper.longitude]}
+                  icon={customIcon}
+                  eventHandlers={{
+                    click: () => handleShipperClick(shipper),
+                  }}
+                >
+                  <Popup>
+                    <img src={shipper.img} alt={shipper.id} />
+                    <h2>{shipper.id}</h2>
+                    <p>Kinh độ: {shipper.latitude}</p>
+                    <p>Vĩ độ: {shipper.longitude}</p>
+                    <p>Biển số xe: {shipper.carindentify}</p>
+                    <p>
+                      Trạng thái: <StatusBadge status={shipper.status} />
+                    </p>
+                  </Popup>
+                </Marker>
+              ))
+            : filterOrders.map((order) => (
+                <Marker
+                  key={order.id}
+                  position={[order.latitude, order.longitude]}
+                  icon={customIcon}
+                >
+                  <Popup>
+                    <img src={order.img} alt={order.id} />
+                    <h2>{order.id}</h2>
+                    <p>Kinh độ: {order.latitude}</p>
+                    <p>Vĩ độ: {order.longitude}</p>
+                    <p>
+                      Trạng thái: <StatusBadge status={order.status} />
+                    </p>
+                  </Popup>
+                </Marker>
+              ))}
         </MarkerClusterGroup>
       </MapContainer>
     </>
